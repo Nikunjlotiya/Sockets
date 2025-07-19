3339203fcf7c05d8860fad94278ff0488b6b6845
nikunj <nikunj.lotiya@einfochips.com>
2025-07-19 17:55:28 +0530

    Listen, Bindand and  Accept Completed

--- server.c
+++ server.c
@@ -14,9 +14,11 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 //Macros
-#define SERVPORT
+
+#define SERVPORT  5000
 #define BUFFERLEN 1024	
 #define CLIENTQ 8 //The number of clients will be in queue while the server is busy
 
@@ -24,7 +26,7 @@ int main(int argc, char * argv[])
 {
 	//Creating a structure element of socket structure
 	struct sockaddr_in ClientAddr;//Structure Which Stores the Client IP and PORT number after accpeting the connection
-	struct sockaddr_in ServeAddr;//Structure which store s the Server IP and PORT number
+	struct sockaddr_in ServerAddr;//Structure which store s the Server IP and PORT number
 
 	//Created Sockets descriptors
 	int32_t iClientFd = 0;
@@ -35,22 +37,22 @@ int main(int argc, char * argv[])
 	char aBuffer[BUFFERLEN];
 	
 	//Client socket length 
-	socketlen_t iClientSocLen;
+	socklen_t iClientSocLen;
 	//Fill the Server Structure with its element i.e. Socket type,ipaddress, port number
-	ServAddr.sin_family = AF_INET;
-	ServAddr.sin_addr.s_addr = INADDR_ANY;
-	ServAddr.sin_port = htons(SERVPORT);
+	ServerAddr.sin_family = AF_INET;
+	ServerAddr.sin_addr.s_addr = INADDR_ANY;
+	ServerAddr.sin_port = htons(SERVPORT);
 
 	//Create the socket
 	iServerFd = socket(AF_INET,SOCK_STREAM,0);
 	if(iServerFd == -1)
 	{
 		printf("Server Socket Creation Failed\n");
-		exit();
+		exit(0);
 	}
 	
 	//Bining the socket with the ip address and port number
-	bind(iServerFd,(struct sockaddr *)&ServAddr,sizeof(ServAddr));
+	bind(iServerFd,(struct sockaddr *)&ServerAddr,sizeof(ServerAddr));
 	
 	//Placin the socket in listening mode
 	listen(iServerFd,CLIENTQ);
@@ -58,6 +60,7 @@ int main(int argc, char * argv[])
 	for(;;)
 	{
 		iClientSocLen = sizeof(ClientAddr);
-		iAgentFd = accept(iServeFd,(struct Sockaddr *)&ClientAddr,&iClientSocLen);
+		iAgentFd = accept(iServerFd,(struct sockaddr *)&ClientAddr,&iClientSocLen);
 
+}	
 }
